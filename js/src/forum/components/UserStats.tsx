32546247642cf2d68a50a1da5cb3c6e85624d54c
Lady-Byron <samantha-everett@outlook.com>
2025-10-29 14:39:28 -0400

    Update UserStats.tsx

--- js/src/forum/components/UserStats.tsx
+++ js/src/forum/components/UserStats.tsx
@@ -19,43 +19,58 @@ export default class UserStats extends Component<UserStatsAttrs> {
     super.oninit(vnode);
     this.userStat = null;
     this.getUserStat();
-
   }
 
   view(vnode: Mithril.Vnode<ComponentAttrs, this>): Mithril.Children {
     if (!this.userStat) {
       return <div></div>;
     }
-    const { data } = this.attrs.user
+
+    const { data } = this.attrs.user;
+
+    // Money（保持不变）
     const moneyName = app.translator.trans('justoverclock-stats.forum.moneyName');
+    const userMoney = data.attributes.money ?? 0;
+    const moneyImg = `${app.forum.attribute('baseUrl')}/assets/extensions/justoverclock-stats/money1.png`;
+
+    // Stories（改为可选：只有当 storyCount 存在且非 null/undefined 时才渲染）
     const storiesName = app.translator.trans('justoverclock-stats.forum.storyCount');
-    const userMoney = data.attributes.money ?? 0
-    const userStoriesCount = data.attributes.storyCount ?? 0
-    const moneyImg =  `${app.forum.attribute('baseUrl')}/assets/extensions/justoverclock-stats/money1.png`
-    const storiesImg =  `${app.forum.attribute('baseUrl')}/assets/extensions/justoverclock-stats/storiespng.png`
-    const canEditStats: boolean = data.attributes?.canEditStats
+    const hasStoriesAttr = Object.prototype.hasOwnProperty.call(data.attributes || {}, 'storyCount')
+                           && data.attributes.storyCount !== null
+                           && data.attributes.storyCount !== undefined;
+    const userStoriesCount = hasStoriesAttr ? data.attributes.storyCount : 0;
+    const storiesImg = `${app.forum.attribute('baseUrl')}/assets/extensions/justoverclock-stats/storiespng.png`;
+
+    const canEditStats: boolean = data.attributes?.canEditStats;
 
     return (
       <div className='show-stats-wrapper'>
         <div className='user-stats-fe show-stats-container'>
+          {/* Money 一直显示 */}
           <SingleUserStat
             name={moneyName}
             img={app.forum.attribute('justoverclock-stats.moneyImg') || moneyImg}
             alt={moneyName}
             value={userMoney}
           />
-          <SingleUserStat
-            name={storiesName}
-            img={app.forum.attribute('justoverclock-stats.storiesImg') || storiesImg}
-            alt={storiesName}
-            value={userStoriesCount}
-          />
-          {this.userStat.data && this.userStat.data.map((stat: UserStat) => {
-            const baseStat: BaseStat | null | undefined = this.userStat && this.userStat.included.find(
-              baseStat => baseStat.id.toString() === stat.attributes.baseStatId.toString()
-            );
-            const imgPath = baseStat ? baseStat.attributes.img : "";
 
+          {/* Stories：只有在属性存在时才显示（从而解除对 profile-stories 的必需依赖） */}
+          {hasStoriesAttr && (
+            <SingleUserStat
+              name={storiesName}
+              img={app.forum.attribute('justoverclock-stats.storiesImg') || storiesImg}
+              alt={storiesName}
+              value={userStoriesCount}
+            />
+          )}
+
+          {/* 自定义 BaseStats（保持不变） */}
+          {this.userStat.data && this.userStat.data.map((stat: UserStat) => {
+            const baseStat: BaseStat | null | undefined =
+              this.userStat && this.userStat.included.find(
+                baseStat => baseStat.id.toString() === stat.attributes.baseStatId.toString()
+              );
+            const imgPath = baseStat ? baseStat.attributes.img : "";
 
             return (
               <SingleUserStat
